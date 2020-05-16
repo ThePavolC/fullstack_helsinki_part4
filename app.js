5ebe115a53a96c8dbad5e487063e103175408904
Pavol Celuch <pavol.celuch@udemy.com>
2020-05-16 11:45:52 +0100

    part4_d_exercises

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const config = require("./utils/config");
 const express = require("express");
+require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const blogRouter = require("./controllers/blog");
+const userRouter = require("./controllers/user");
+const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -25,8 +28,11 @@ app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
